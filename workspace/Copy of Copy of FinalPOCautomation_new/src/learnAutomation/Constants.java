c95d2325566f8dd2c5448c315312ac75a6a4501a
Siddhant123abc <siddhantsahoo23@gmail.com>
2017-04-12 15:22:03 +0530

    Testing for github

--- workspace/Copy of Copy of FinalPOCautomation_new/src/learnAutomation/Constants.java
+++ workspace/Copy of Copy of FinalPOCautomation_new/src/learnAutomation/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String Test_Data_Sheet="resources\\TestData4.xls";
     public static final String FinalReport_Data_Sheet="resources\\DetailedTestSuiteReport.xls";
    public static final String filePath = "resources\\Locator.json";	
-	public static final String abc="";
+	public static final String abc="testing for github";
 }
 
 
