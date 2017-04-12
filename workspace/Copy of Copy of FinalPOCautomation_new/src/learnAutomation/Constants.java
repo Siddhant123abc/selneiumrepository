1425ad217ad0bbc03372c3a192ce42d34963127c
Siddhant123abc <siddhantsahoo23@gmail.com>
2017-04-12 15:14:20 +0530

    testing for commit

---
+++ workspace/Copy of Copy of FinalPOCautomation_new/src/learnAutomation/Constants.java
@@ -0,0 +1,19 @@
+package learnAutomation;
+
+public class Constants {
+
+	
+	/*public static final String Master_Data_Sheet="D:\\automation\\POC_Automation_updatedasperRasmita\\MasterTestSuitefordemo.xls";
+    public static final String Test_Data_Sheet="D:\\automation\\POC_Automation_updatedasperRasmita\\TestData4.xls";
+    public static final String FinalReport_Data_Sheet="D:\\automation\\POC_Automation_updatedasperRasmita\\DetailedTestSuiteReport.xls";
+   public static final String filePath = "D:\\automation\\POC_Automation_updatedasperRasmita\\Locator.json";*/
+	
+	public static final String Master_Data_Sheet="resources\\MasterTestSuitefordemo.xls";
+    public static final String Test_Data_Sheet="resources\\TestData4.xls";
+    public static final String FinalReport_Data_Sheet="resources\\DetailedTestSuiteReport.xls";
+   public static final String filePath = "resources\\Locator.json";	
+	public static final String abc="";
+}
+
+
+
